8ab8f620942f1c7fa314f9a85349ce22ff50d435
Tim Carey-Smith <tim@spork.in>
2010-09-23 01:38:47 +1200

    start the xserver

--- run.rb
+++ run.rb
@@ -3,5 +3,13 @@ Capybara.default_driver = :selenium
 
 include Capybara
 
-visit('http://google.com/')
-save_and_open_page
+ENV["DISPLAY"] = ":20"
+
+# Check if Xvfb is running (for headless Firefox) and run it again
+if %x[ps -ef | grep "xinit" | grep -v grep].empty?
+  auth_file = "#{ENV['HOME']}/X99.cfg"
+  system "echo 'localhost' > #{auth_file}"
+  system "xinit -- `which Xvfb` #{ENV['DISPLAY']} -screen 0 1024x768x24 -auth #{auth_file} 2>&1 > xinit.log &"
+end
+
+visit 'http://google.com/'
